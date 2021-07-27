a4c16948971c71f36c5dc588f4a8b924051f5fa6
numatsumu <auchinumax@gmail.com>
2021-07-27 14:38:18 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configue_permitted_parameters, if: :devise_contoroller?
+  before_action :configue_permitted_parameters, if: :devise_controller?
 
   private
   def configue_permitted_parameters
